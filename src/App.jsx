ee496696dfefd7186715f4359a6d8c16345ec3bb
Arafat Patwary <farabipsk@gmail.com>
2025-12-03 15:14:30 +0600

    Use Page Layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 
 import './App.css'
 import Donation from './components/Donation/donation'
+import Page from './components/PageLayout/Page'
+import PageHub from './components/PageLayout/PageHub'
 
 function App() {
 
   return (
     <>
       <div>
-       <Donation/>
+       {/* <Donation/> */}
+       <PageHub/>
+       
       </div>
       
     </>
