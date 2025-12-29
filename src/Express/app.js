07ca5fef39a43341805d63eae06752142329d42f
Arafat Patwary <farabipsk@gmail.com>
2025-12-29 08:18:10 +0600

    create express route

--- src/Express/app.js
+++ src/Express/app.js
@@ -1,9 +1,26 @@
-const express = require('express')
+import express from 'express';
 const app = express();
 
 app.get('/', (req,res)=> {
     res.send('Hello Express!')
 });
+
+app.get('/about', (req,res)=> {
+    res.send('All About Express!')
+});
+
+app.get('/contact', (req,res)=> {
+    res.send('Contact Express!')
+});
+
+app.get('/product', (req,res)=> {
+    res.send('Product Lib!')
+});
+
+//? Sign up route
+app.post('/sign', (req,res)=>{
+    res.send('Register route')
+})
 
 app.listen(3000, ()=>{
     console.log(' Server is running on port http://localhost:3000')
